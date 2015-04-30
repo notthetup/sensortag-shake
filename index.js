4b5e0764990224e885a0560777a7410fa1c61090
Chinmay Pendharkar <notthetup@gmail.com>
2015-04-30 08:13:35 +0800

    making in module

--- index.js
+++ index.js
@@ -1,73 +1,69 @@
-var util = require('util');
+var EventEmitter = require("events").EventEmitter;
 var async = require('async');
-var SensorTag = require('sensortag');
-var express = require('express')
-var app = express();
-
-var server = app.listen(3000, function () {
-  console.log('Control the servo at http://localhost:' + server.address().port);
-})
-var io = require('socket.io').listen(server);
-var sockets = [];
-
-app.use(express.static(__dirname));
-
-io.on('connection', function (socket) {
-  sockets.push(socket);
-})
+var util = require('util');
+var SensorTag = require('SensorTag');
 
 
-var prev = 0;
-var shakeX = [];
-var shakeXDiff = [];
-var prevTime = new Date().getTime();
-var prevSpeed = 0;
-var alpha = 0.7;
+function SensorTagShake (){
 
-SensorTag.discover(function(sensorTag) {
-  console.log('discovered: ' + sensorTag);
+  var prev = 0;
+  var shakeX = [];
+  var shakeXDiff = [];
+  var prevTime = new Date().getTime();
+  var prevSpeed = 0;
+  var alpha = 0.7;
+  var self = this;
 
-  sensorTag.on('disconnect', function() {
-    console.log('disconnected!');
-    process.exit(0);
-  });
+  EventEmitter.call(this);
 
-  async.series([
-      function(callback) {
-        console.log('connected!');
-        sensorTag.connectAndSetUp(callback);
-      },
+  SensorTag.discover(function(sensorTag) {
+    console.log('discovered: ' + sensorTag);
 
-      function(callback) {
-        // console.log('enableAccelerometer');
-        sensorTag.enableAccelerometer(callback);
-      },
+    sensorTag.on('disconnect', function() {
+      console.log('disconnected!');
+      process.exit(0);
+    });
 
-      function(callback) {
-         sensorTag.on('accelerometerChange', function(x, y, z) {
-          var diff = x - prev;
-          prev = x;
-          if (diff > 0.7){
-            var currTime = new Date().getTime();
-            var tDiff = currTime - prevTime;
-            if (tDiff < 190){
-              tDiff = 190;
+    async.series([
+        function(callback) {
+          console.log('connected!');
+          sensorTag.connectAndSetUp(callback);
+        },
+
+        function(callback) {
+          // console.log('enableAccelerometer');
+          sensorTag.enableAccelerometer(callback);
+        },
+
+        function(callback) {
+           sensorTag.on('accelerometerChange', function(x, y, z) {
+            var diff = x - prev;
+            prev = x;
+            if (diff > 0.7){
+              var currTime = new Date().getTime();
+              var tDiff = currTime - prevTime;
+              if (tDiff < 190){
+                tDiff = 190;
+              }
+              var speed = 250/tDiff;
+              var smoothenedSpeed = ((1-alpha)*speed)+(alpha*prevSpeed);
+              prevSpeed = smoothenedSpeed;
+              prevTime = currTime;
+              // console.log(smoothenedSpeed, speed,diff, tDiff);
+              self.emit('shake', smoothenedSpeed);
             }
-            var speed = 250/tDiff;
-            var smoothenedSpeed = ((1-alpha)*speed)+(alpha*prevSpeed);
-            prevSpeed = smoothenedSpeed;
-            prevTime = currTime;
-            console.log(smoothenedSpeed, speed,diff, tDiff);
-            sockets.forEach(function(eachSocket) {
-              eachSocket.emit('speedChange', {speed:smoothenedSpeed});
-            })
-          }
-        });
-        sensorTag.setAccelerometerPeriod(200, function(error) {
-          console.log('notifyAccelerometer');
-          sensorTag.notifyAccelerometer();
-        });
-      }
-    ]
-  );
-});
+          });
+          sensorTag.setAccelerometerPeriod(200, function(error) {
+            console.log('notifyAccelerometer');
+            sensorTag.notifyAccelerometer();
+          });
+        }
+      ]
+    );
+  });
+}
+
+util.inherits(SensorTagShake, EventEmitter);
+module.exports = SensorTagShake;
+
+
